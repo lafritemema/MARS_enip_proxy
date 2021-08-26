e71d586cb2eed6dbc11f277ff2844085d84351da
lafritemema <lafritemema@gmail.com>
2021-08-26 16:17:07 +0200

    integrate lint validation

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'root': true,
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'max-len': ['error', {'ignoreComments': true}],
+  },
+};
